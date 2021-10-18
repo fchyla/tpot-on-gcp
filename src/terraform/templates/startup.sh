660233bb1c6795e12f5e0e90ce490109274ee57a
Filip Chyla <filip@chy.la>
2021-10-18 16:14:20 +0200

    merge

--- src/terraform/templates/startup.sh
+++ src/terraform/templates/startup.sh
@@ -1,4 +1,6 @@
-if [[ -z $(dpkg -l | grep "cloud-init" ) ]]
+#!/bin/bash
+
+if ! dpkg -l | grep -q "cloud-init"
 then
 	echo "cloud-init not installed, installing and reseting instance"
 	apt install -y cloud-init
